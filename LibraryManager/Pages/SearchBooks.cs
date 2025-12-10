1b916b7852f7444a632f75ccb02c0b2b77f4da63
Jefernater58 <rayner.freddie@btinternet.com>
2025-12-10 13:54:02 +0000

    Update SearchBooks

--- LibraryManager/Pages/SearchBooks.cs
+++ LibraryManager/Pages/SearchBooks.cs
@@ -1,25 +1,78 @@
-﻿namespace LibraryManager.Pages
+﻿using Microsoft.Data.Sqlite;
+
+namespace LibraryManager.Pages
 {
+
     public partial class SearchBooks : UserControl
     {
+
         public SearchBooks()
         {
             InitializeComponent();
 
-            // temporary to show the design
-            dataGrid.Rows.Add(0, "1984", "George Orwell", "5/5", true);
-            dataGrid.Rows.Add(1, "To Kill a Mockingbird", "Harper Lee", "4/5", false);
-            dataGrid.Rows.Add(2, "The Great Gatsby", "F. Scott Fitzgerald", "3/5", true);
-            dataGrid.Rows.Add(3, "Brave New World", "Aldous Huxley", "4/5", true);
-            dataGrid.Rows.Add(4, "Moby Dick", "Herman Melville", "2/5", false);
-            dataGrid.Rows.Add(5, "Pride and Prejudice", "Jane Austen", "5/5", true);
-            dataGrid.Rows.Add(6, "The Catcher in the Rye", "J.D. Salinger", "3/5", false);
-            dataGrid.Rows.Add(7, "The Hobbit", "J.R.R. Tolkien", "5/5", true);
-            dataGrid.Rows.Add(8, "Fahrenheit 451", "Ray Bradbury", "4/5", true);
-            dataGrid.Rows.Add(9, "Jane Eyre", "Charlotte Brontë", "4/5", false);
-            dataGrid.Rows.Add(10, "The Odyssey", "Homer", "5/5", true);
+            BookEntry[] data = loadData();
 
-            // TODO: add search functionality to search the database for books
+            // add the books to the displayed list
+            for (int i = 0; i < data.Length; i++)
+            {
+                BookEntry entry = data[i];
+
+                // TODO: calculate average rating
+                dataGrid.Rows.Add(entry.Id, entry.Title, entry.Author, "nil", !entry.CheckedOut);
+            }
+
+            // TODO: make search function work
+        }
+
+        BookEntry[] loadData()
+        {
+            // connect to the database
+            string connectionString = "Data Source=db.sqlite";
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                // create a command to get all books from the database table
+                var getBooksCmd = connection.CreateCommand();
+                getBooksCmd.CommandText =
+                @"
+                    SELECT * FROM Books;
+                ";
+
+                // iterate through results and create objects to use in code
+                BookEntry[] allRecords;
+                List<BookEntry> list = new List<BookEntry>();
+                using (var reader = getBooksCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new BookEntry(reader.GetInt64(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetBoolean(4)));
+                    }
+
+                    allRecords = list.ToArray();
+                }
+
+                // return the data
+                return allRecords;
+            }
+        }
+    }
+
+    public class BookEntry
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string RatingsJson { get; set; }
+        public bool CheckedOut { get; set; }
+
+        public BookEntry(long Id, string Title, string Author, string RatingsJson, bool CheckedOut)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Author = Author;
+            this.RatingsJson = RatingsJson;
+            this.CheckedOut = CheckedOut;
         }
     }
 }
